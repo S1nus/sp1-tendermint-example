afc2777b9ed19eb058239a9b5043dabf251adc9c
c-node <chuckhead7@gmail.com>
2024-10-01 00:11:39 -0700

    ugh i need to re-do the tia_finder

--- operator/bin/json_pruner.rs
+++ operator/bin/json_pruner.rs
@@ -1,4 +1,9 @@
+use core::time::Duration;
+use serde_json;
 use std::{fs, path::Path};
+use tendermint_light_client_verifier::{
+    options::Options, types::LightBlock, ProdVerifier, Verdict, Verifier,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -16,7 +21,55 @@ async fn main() -> anyhow::Result<()> {
             files.push(filename.to_string());
         }
     }
-    files.sort_by(|a, b| a.parse::<u32>().cmp(&b.parse::<u32>()));
-    println!("{:?}", files);
+    files.sort_by(|a, b| a.parse::<u32>().unwrap().cmp(&b.parse::<u32>().unwrap()));
+    for i in 0..files.len() {
+        let file = fs::File::open(format!("{}.json", files[i])).unwrap();
+        let reader = std::io::BufReader::new(file);
+        let first_block: LightBlock = serde_json::from_reader(reader).unwrap();
+        let mut closest_reject_height: Option<u64> = None;
+        let mut num_dels = 0;
+        for j in i + 1..files.len() {
+            let file2 = fs::File::open(format!("{}.json", files[j])).unwrap();
+            let reader2 = std::io::BufReader::new(file2);
+            let second_block: LightBlock = serde_json::from_reader(reader2).unwrap();
+            let vp = ProdVerifier::default();
+            let opt = Options {
+                trust_threshold: Default::default(),
+                // 2 week trusting period.
+                trusting_period: Duration::from_secs(14 * 24 * 60 * 60),
+                clock_drift: Default::default(),
+            };
+            let verify_time = first_block.time() + Duration::from_secs(20);
+            let verdict = vp.verify_update_header(
+                second_block.as_untrusted_state(),
+                first_block.as_trusted_state(),
+                &opt,
+                verify_time.unwrap(),
+            );
+            match verdict {
+                Verdict::Success => {
+                    println!("this ok");
+                    num_dels += 1;
+                }
+                _ => {
+                    closest_reject_height = Some(second_block.height().value());
+                    println!(
+                        "closest reject height for {} is {}",
+                        first_block.height(),
+                        second_block.height()
+                    );
+                    break;
+                }
+            }
+        }
+        println!("height {} has {} dels", first_block.height(), num_dels);
+    }
+    /*for (i, f) in files.iter().enumerate() {
+        let file = fs::File::open(format!("{}.json", f)).unwrap();
+        let reader = std::io::BufReader::new(file);
+        let deserialized: LightBlock = serde_json::from_reader(reader).unwrap();
+        for (j, f2) in files.
+        }
+    }*/
     Ok(())
 }
